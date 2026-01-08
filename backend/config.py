48caf79570e88b91cf807cbff14f9cc6f50c34e4
contatomadameka-maker <contatomadameka@gmail.com>
2026-01-08 07:50:16 -0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -23,7 +23,7 @@ class BotConfig:
 
     # ===== API DERIV =====
     # COLE SEU TOKEN AQUI ENTRE AS ASPAS ↓
-    API_TOKEN = "6ERlrkXsO23Egc5"
+    API_TOKEN = "vPu2lzCDshSNKIp"
     APP_ID = "1089"
 
     # ===== MERCADO =====
